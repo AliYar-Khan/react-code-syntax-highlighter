cbe0522cd5916a6b716951cfed42f01471e18c4e
Ali Yar Khan <aliyark145@gmail.com>
2025-06-28 14:56:44 +0500

    fixes the highlighting problem and added screenshots

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/react-code-syntax-highlighter/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "text-zinc-500",
+    "italic",
+    "text-yellow-400",
+    "text-blue-400",
+    "font-semibold",
+    "text-green-400",
+    "text-purple-400",
+    "text-teal-300",
+    "text-white",
+  ],
   theme: {
     extend: {},
   },
